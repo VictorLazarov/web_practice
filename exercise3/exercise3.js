b91d832405d5058a1da9f5b223173be8f8b627c8
Viktor Lazarov <vlazarov278@gmail.com>
2024-10-22 09:00:26 +0300

    Refactor registration form event listener

--- exercise3/exercise3.js
+++ exercise3/exercise3.js
@@ -2,8 +2,10 @@ const registrationForm = document.getElementById('registrationForm');
 
 registrationForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const formData = new FormData(registrationForm);
+    /* const formData = new FormData(registrationForm);
     const data = Object.fromEntries(formData);
-    console.log(data);
-    }
-);
+    console.log(data); */
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log(username, password);
+});
